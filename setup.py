0953420451cdc6d5a4088c2ae35b0ee5d935cbe3
spex <murderprivacy@gmail.com>
2026-03-14 19:30:08 -0500

    v4.0.0 working release setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='hashitout',
     version='4.0.0',
-    description='Elite decoder, reverser, file rebuilder, stego scanner, and URL content analyzer',
-    url='https://github.com/RRSWSEC/Hash-It-Out',
     packages=find_packages(),
+    install_requires=[],
+    entry_points={'console_scripts': ['hashitout=hashitout:main']},
     python_requires='>=3.7',
-    entry_points={
-        'console_scripts': [
-            'hashitout=hashitout:main',
-        ],
-    },
-    package_data={
-        '': ['wordlists/*.txt'],
-    },
 )
